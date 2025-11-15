1cf4d2a9493db46a5a31599a7c2d61a78a0dc823
jasonu80 <79412658+jasonu80@users.noreply.github.com>
2025-11-16 10:35:12 +1100

    Add solution script to change file ownership and permissions

---
+++ Change File Ownership/solution.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+touch target_file # solution of first problem
+sudo chown user1:group1 target_file # Solution of second problem
+sudo chmod u+x,o-r target_file # Solution of third problem
